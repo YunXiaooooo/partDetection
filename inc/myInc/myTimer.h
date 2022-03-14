c02583bf0f3652d74c093a8268487c882316a9de
YunXiaooooo <1924835157@qq.com>
2022-03-14 13:48:41 +0800

    修改定时器m_stop状态读写以及条件变量锁的释放，避免modbus超时导致无法正常退出

--- inc/myInc/myTimer.h
+++ inc/myInc/myTimer.h
@@ -24,18 +24,20 @@ public:
 
     void start(int interval, std::function<void(modbusRtuMaster*)> task, modbusRtuMaster* mbMaster)
     {
-        if (!m_stop)
+        if (!readM_stop())
             return;
-        m_stop = false;
+        setM_stop(false);//启动时，将m_stop设为false
 
         m_future = std::async(std::launch::async, [this, interval,task, mbMaster]()
             {
                 auto start = std::chrono::high_resolution_clock::now(); //当前时间
                 auto end = start + std::chrono::milliseconds(interval); //结束时间
-                while (!m_stop)
+                while (!readM_stop())
                 {
+                    
                     std::unique_lock<std::mutex> lock(m_mutex);
                     const auto status = m_cv.wait_until(lock, end);
+                    lock.unlock();//尽早释放锁，避免modbus通信超时使得锁长时间被锁定，退出时stop内无法修改m_stop状态
                     //const auto status = m_cv.wait_for(lock, std::chrono::milliseconds(interval));
                     if (status == std::cv_status::timeout)
                     {
@@ -49,19 +51,26 @@ public:
 
     void stop()
     {
-        if (m_stop)
+        if (readM_stop())
             return;
 
-        {
-            std::unique_lock<std::mutex> lock(m_mutex);
-            m_stop = true;
-        }
+        setM_stop(true);
         m_cv.notify_one();
         m_future.wait();
     }
 
 private:
     bool m_stop{ true };
+    bool readM_stop()
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_stop;
+    }
+    void setM_stop(bool tf)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_stop = tf;
+    }
     std::mutex m_mutex;
     std::condition_variable m_cv;
     std::future<void> m_future;
